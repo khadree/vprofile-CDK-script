4a9f8227a134e3e5dd30b44dee136d7f04c1dee3
khadreeg <khadreegeorge@gmail.com>
2025-10-03 15:34:55 +0100

    Initial commit

---
+++ test/vprofile-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as VprofileApp from '../lib/vprofile-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/vprofile-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new VprofileApp.VprofileAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
